57b7ab0d4a90466330d44ea4c837c30dfb86ac9b
sebastian <sebastian.pellitero@gmail.com>
2024-06-24 23:01:24 +0200

    added details and summary styles

--- src/components/mainPage/HomePage.tsx
+++ src/components/mainPage/HomePage.tsx
@@ -13,32 +13,90 @@ const MainSection = styled.section`
     h1 {
         color: ${pageColors.darkText};
     }
+    /* details {
+        border: 1px solid #aaa;
+        border-radius: 4px;
+        padding: 0.5em 0.5em 0;
+    }
 
-    `
+    summary {
+        font-weight: bold;
+        margin: -0.5em -0.5em 0;
+        padding: 0.5em;
+    }
+
+    details[open] {
+        padding: 0.5em;
+    }
+
+    details[open] summary {
+        border-bottom: 1px solid #aaa;
+        margin-bottom: 0.5em;
+    } */
+
+    details {
+        width: 80%;
+        margin: 0 auto ;
+        background: #282828;
+        margin-bottom: .5rem;
+        box-shadow: 0 .1rem 1rem -.5rem rgba(0,0,0,.4);
+        border-radius: 5px;
+        overflow: hidden;
+    }
+
+    summary {
+        padding: 1rem;
+        display: block;
+        background: #333;
+        padding-left: 2.2rem;
+        position: relative;
+        cursor: pointer;
+        user-select: none;
+    }
+
+    summary:before {
+        content: '';
+        border-width: .4rem;
+        border-style: solid;
+        border-color: transparent transparent transparent #fff;
+        position: absolute;
+        top: 1.3rem;
+        left: 1rem;
+        transform: rotate(0);
+        transform-origin: .2rem 50%;
+        transition: .25s transform ease;
+    }
+
+    details[open] > summary:before {
+        transform: rotate(90deg);
+    }
+
+
+    details summary::-webkit-details-marker {
+        display:none;
+    }
+
+    details > ul {
+        padding-bottom: 1rem;
+        margin-bottom: 0;
+    }
+`
 
 const CanvasWrapper = styled.div`
     background-color: white;
     border: 5px solid blue;
 `
-export default function HomePage() {
 
+export default function HomePage() {
     return (
         <>
             <MainSection>
                 <h1>Select the algorithm that you want to see</h1>
-                <ul>
-                    <li>
-                        <pre>
-                            <code>Algoritmo 1   </code>
-                        </pre>
-                    </li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
+                <details>
+                    <summary>Algorithm 1</summary>
+                    <code> export const depthFirstValuesRecursive = (root: INode | null): Array</code>
+                </details>
+
 
                 <p>El dibujito:</p>
                 <CanvasWrapper>
